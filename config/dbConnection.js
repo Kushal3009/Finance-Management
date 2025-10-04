f6505d9d40041ece27088d634340f2346a6a4955
Kushal3009 <kushalpadhiyar030@gmail.com>
2025-10-05 00:00:33 +0530

    Refactor database connection configuration and update dependencies

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,24 +4,26 @@ import logger from '../logger/logger.js';
 
 dotenv.config();
 
+const DB_DIALECT = process.env.DB_DIALECT || 'mssql';
+
 
 // Initialize Sequelize with MSSQL configuration
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mssql',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: DB_DIALECT || 'mssql',
     logging: false,
-    port: 1433,
 });
 
 // Test the database connection
 export const connectDB = async () => {
     try {
         await sequelize.authenticate();
-        logger.info('Connection to MSSQL has been established successfully.');
+        logger.info(`Connection to ${DB_DIALECT} has been established successfully.`);
     } catch (error) {
+        console.log(error)
         logger.error('Unable to connect to the database:', error);
     }
 }
